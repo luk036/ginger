d1790830da72fbc0f1468b3f86e4a637931907e3
Wai-Shing Luk <luk036@gmail.com>
2025-04-02 12:23:11 +0800

    add test_extract_autocorr()

--- tests/test_autocorr.py
+++ tests/test_autocorr.py
@@ -1,5 +1,12 @@
 from ginger.autocorr import extract_autocorr, initial_autocorr, pbairstow_autocorr
 from ginger.rootfinding import Options, find_rootq
+from ginger.vector2 import Vector2
+
+
+def test_extract_autocorr():
+    vr = extract_autocorr(Vector2(1, -4))
+    assert vr.x == 0.25
+    assert vr.y == -0.25
 
 
 def test_autocorr():
